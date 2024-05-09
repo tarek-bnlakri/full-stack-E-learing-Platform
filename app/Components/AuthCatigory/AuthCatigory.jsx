46bb4164105208aeb1a17ee459abb9037ebc689b
tarek-bnlakri <143798415+tarek-bnlakri@users.noreply.github.com>
2024-05-09 09:37:17 +0100

    Update AuthCatigory.jsx

--- app/Components/AuthCatigory/AuthCatigory.jsx
+++ app/Components/AuthCatigory/AuthCatigory.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 
 const getData= async()=>{
-  const res= await fetch(`http://localhost:3000/api/catigory`,{cache:"no-cache"})
+  const res= await fetch(`/api/catigory`,{cache:"no-cache"})
   if(!res.ok){
     throw new Error(`Error fetching`)
   }
