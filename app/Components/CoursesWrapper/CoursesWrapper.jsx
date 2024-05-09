83db8291f7806bbb6008ca4d6cab4e16a372c192
tarek-bnlakri <143798415+tarek-bnlakri@users.noreply.github.com>
2024-05-09 10:00:24 +0100

    Update CoursesWrapper.jsx

--- app/Components/CoursesWrapper/CoursesWrapper.jsx
+++ app/Components/CoursesWrapper/CoursesWrapper.jsx
@@ -8,7 +8,7 @@ async function CoursesWrapper({catid}) {
   const session = await getAuthSession()
   const getData=async(catid)=>{
     try {
-          const res= await fetch(`https://full-stack-e-learing-platform.vercel.app/api/course?catid=${catid || ""}&userEmail=${session.user.email}`,{cache:"no-store"})
+          const res= await fetch(`https://full-stack-e-learing-platform.vercel.app/api/course?catid=${catid || ""}&userEmail=${session.user.email}`,{cache:"no-store"},{method:"GET"})
           // if(!res.ok){
           //   throw new Error("Couldn't connect to server")
           // }
