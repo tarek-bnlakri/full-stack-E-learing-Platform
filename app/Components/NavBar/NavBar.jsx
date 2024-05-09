30351196dcde53493974873c1dfbc3396c92aef5
tarek-bnlakri <143798415+tarek-bnlakri@users.noreply.github.com>
2024-05-09 16:22:09 +0100

    Update NavBar.jsx

--- app/Components/NavBar/NavBar.jsx
+++ app/Components/NavBar/NavBar.jsx
@@ -4,14 +4,14 @@ import UnAuthLinks from '../UnAuthLinks/UnAuthLinks.jsx'
 import AuthLinks from '../AuthLinks/AuthLinks.jsx'
 import { getAuthSession } from '@/util/auth';
 import Link from 'next/link';
-
+import Image from 'next/image';
 async function NavBar() {
   
   const data= await getAuthSession()
   console.log(data)
   return (
     <div  className='NavBar'>
-        <div className='logo'><img src="/logoLearning.png" alt="" /><Link href={data?"/courses":"/"}><img src="/logoLearning.png" alt="" /></Link></div>
+        <div className='logo'><img src="/logoLearning.png" alt="" /><Link href={data?"/courses":"/"}><Image loading='lazy' width={170} height={170} src="/logoLearning.png" alt="" /></Link></div>
         <div className='right'>
           {data?<AuthLinks data={data}/>:<UnAuthLinks data={data}/>}
         </div>
